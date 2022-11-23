e9754629bdae73667d6c18ead35f42833fc9df27
AzhureRaven <abrahamarthurfendy@gmail.com>
2022-11-23 16:27:32 +0700

    Hapus fungsi tidak perlu

--- app/src/main/java/com/example/ekartumrtscanner/Koneksi.kt
+++ app/src/main/java/com/example/ekartumrtscanner/Koneksi.kt
@@ -80,81 +80,7 @@ object Koneksi {
         return numberFormat.format(this)
     }
 
-    fun invertImage(src: Bitmap): Bitmap? {
-        // create new bitmap with the same attributes(width,height)
-        //as source bitmap
-        val bmOut = Bitmap.createBitmap(src.width, src.height, src.config)
-        // color info
-        var A: Int
-        var R: Int
-        var G: Int
-        var B: Int
-        var pixelColor: Int
-        // image size
-        val height = src.height
-        val width = src.width
-
-        // scan through every pixel
-        for (y in 0 until height) {
-            for (x in 0 until width) {
-                // get one pixel
-                pixelColor = src.getPixel(x, y)
-                // saving alpha channel
-                A = Color.alpha(pixelColor)
-                // inverting byte for each R/G/B channel
-                R = 255 - Color.red(pixelColor)
-                G = 255 - Color.green(pixelColor)
-                B = 255 - Color.blue(pixelColor)
-                // set newly-inverted pixel to output image
-                bmOut.setPixel(x, y, Color.argb(A, R, G, B))
-            }
-        }
-
-        // return final bitmap
-        return bmOut
-    }
-
     //EKartu
-    fun getEKartus(): ArrayList<EKartu>{
-        val query = getConnection().prepareStatement("select * from e_kartu")
-        val result = query.executeQuery()
-        val EKartus = ArrayList<EKartu>()
-        while(result.next()){
-            EKartus.add(EKartu(result.getInt("id_kartu"),
-                result.getString("nama_lengkap"),
-                result.getString("username"),
-                result.getString("password"),
-                result.getString("email"),
-                result.getString("tgl_lahir"),
-                result.getString("kelamin"),
-                result.getString("tgl_register"),
-                result.getDouble("saldo"),
-                result.getInt("status_kartu")
-            ))
-        }
-        return EKartus
-    }
-
-    fun getEKartu(username:String, password:String): EKartu?{
-        val query = getConnection().prepareStatement("select * from e_kartu where (username = '$username' or email = '$username' )and password = '$password' and status_kartu = 1")
-        val result = query.executeQuery()
-        var eKartu: EKartu? = null
-        while(result.next()){
-            eKartu = EKartu(result.getInt("id_kartu"),
-                result.getString("nama_lengkap"),
-                result.getString("username"),
-                result.getString("password"),
-                result.getString("email"),
-                result.getString("tgl_lahir"),
-                result.getString("kelamin"),
-                result.getString("tgl_register"),
-                result.getDouble("saldo"),
-                result.getInt("status_kartu")
-            )
-        }
-        return eKartu
-    }
-
     fun getEKartu(id_kartu:Int): EKartu?{
         val query = getConnection().prepareStatement("select * from e_kartu where id_kartu = ${id_kartu} and status_kartu = 1")
         val result = query.executeQuery()
@@ -175,61 +101,12 @@ object Koneksi {
         return eKartu
     }
 
-    fun checkEKartu(username:String, email:String): EKartu?{
-        val query = getConnection().prepareStatement("select * from e_kartu where (username = '$username' or email = '$email' )")
-        val result = query.executeQuery()
-        var eKartu: EKartu? = null
-        while(result.next()){
-            eKartu = EKartu(result.getInt("id_kartu"),
-                result.getString("nama_lengkap"),
-                result.getString("username"),
-                result.getString("password"),
-                result.getString("email"),
-                result.getString("tgl_lahir"),
-                result.getString("kelamin"),
-                result.getString("tgl_register"),
-                result.getDouble("saldo"),
-                result.getInt("status_kartu")
-            )
-        }
-        return eKartu
-    }
-
-    fun insertEKartu(eKartu: EKartu){
-        val query = getConnection().prepareStatement("insert into e_kartu (nama_lengkap,username,password,email,tgl_lahir,kelamin,saldo) " +
-                "values ('${eKartu.nama_lengkap}','${eKartu.username}','${eKartu.password}','${eKartu.email}','${eKartu.tgl_lahir}','${eKartu.kelamin}',0)")
-        val result = query.executeUpdate()
-    }
-
     fun tambahSaldo(eKartu: EKartu,saldo: Int){
         val query = getConnection().prepareStatement("update e_kartu set saldo = saldo + $saldo where id_kartu = ${eKartu.id_kartu}")
         val result = query.executeUpdate()
     }
 
     //ETiket
-    fun getETikets(eKartu: EKartu): ArrayList<ETiket>{
-        val query = getConnection().prepareStatement("SELECT * from e_tiket where id_kartu = ${eKartu.id_kartu} order by tgl_cetak desc")
-        val result = query.executeQuery()
-        val eTikets = ArrayList<ETiket>()
-        while(result.next()){
-            val eTiket = ETiket(
-                result.getInt("id_tiket"),
-                result.getInt("id_kartu"),
-                result.getInt("id_stasiun_awal"),
-                result.getInt("id_stasiun_akhir"),
-                result.getInt("id_rute"),
-                result.getDouble("harga"),
-                result.getString("tgl_cetak"),
-                result.getString("tgl_masuk"),
-                result.getString("tgl_keluar"),
-                result.getInt("mode_tiket"),
-                result.getInt("status_tiket")
-            )
-            eTikets.add(eTiket)
-        }
-        return eTikets
-    }
-
     fun getETiket(id_tiket:Int): ETiket?{
         val query = getConnection().prepareStatement("SELECT * from e_tiket where id_tiket = ${id_tiket}")
         val result = query.executeQuery()
@@ -317,20 +194,6 @@ object Koneksi {
     }
 
     //Rute
-    fun getRute(eTiket: ETiket): Rute?{
-        val query = getConnection().prepareStatement("select * from rute where id_rute = ${eTiket.id_rute}")
-        val result = query.executeQuery()
-        var rute: Rute? = null
-        while(result.next()){
-            rute = Rute(result.getInt("id_rute"),
-                result.getString("nama_rute"),
-                result.getDouble("ppm"),
-                result.getInt("status_rute")
-            )
-        }
-        return rute
-    }
-
     fun getRutes(): ArrayList<Rute>{
         val query = getConnection().prepareStatement("select * from rute where status_rute != 2")
         val result = query.executeQuery()
@@ -347,34 +210,6 @@ object Koneksi {
     }
 
     //Stasiun
-    fun getStasiunAwal(eTiket: ETiket): Stasiun?{
-        val query = getConnection().prepareStatement("select * from stasiun where id_stasiun = ${eTiket.id_stasiun_awal}")
-        val result = query.executeQuery()
-        var stasiun: Stasiun? = null
-        while(result.next()){
-            stasiun = Stasiun(result.getInt("id_stasiun"),
-                result.getString("alamat"),
-                result.getString("nama_stasiun"),
-                result.getInt("status_stasiun")
-            )
-        }
-        return stasiun
-    }
-
-    fun getStasiunAkhir(eTiket: ETiket): Stasiun?{
-        val query = getConnection().prepareStatement("select * from stasiun where id_stasiun = ${eTiket.id_stasiun_akhir}")
-        val result = query.executeQuery()
-        var stasiun: Stasiun? = null
-        while(result.next()){
-            stasiun = Stasiun(result.getInt("id_stasiun"),
-                result.getString("alamat"),
-                result.getString("nama_stasiun"),
-                result.getInt("status_stasiun")
-            )
-        }
-        return stasiun
-    }
-
     fun getStasiun(dRute: DRute): Stasiun?{
         val query = getConnection().prepareStatement("select * from stasiun where id_stasiun = ${dRute.id_stasiun}")
         val result = query.executeQuery()
